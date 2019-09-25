45ce4fea5c71673b8b8f1aef94e60eb4acf1cd21
Bren <brennenbates2859@gmail.com>
2019-09-25 15:39:46 -0600

    Create chat.js
    Creating basics for chat messaging

---
+++ assets/javascript/chat.js
@@ -0,0 +1,19 @@
+
+
+//Functions for chat messaging
+function sendChatMessage() { 
+    ref = firebase.database().ref("/chat");
+    messageField = document.querySelector("#chat-message");
+
+    ref.push().set({
+        name: firebase.auth().currentUser.displayName,
+        message: messageField.value
+    })
+}
+
+ref = firebase.database().ref("/chat");
+
+ref.on("child_added", function(snapshot) { 
+    var message = snapshot.val();
+    addChatMessage(message.name, message.message);
+});
