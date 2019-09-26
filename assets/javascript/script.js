f9ea38db135a14557b4e7eb196e9f7f6f3e30565
Bren-lap <brennenbates2859@gmail.com>
2019-09-26 03:14:35 -0600

    Update script.js
    Creating log in (with modal) and log out button functionality.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+//set the log out button to a display of none.  After a user is logged in, will swap this with the login button.
+$('#logOutButton').css('display', 'none');
 
  var firebaseConfig = {
     apiKey: "AIzaSyC5mbxor2QPARgNB8-pp4f02jGxgzeAAhY",
@@ -15,161 +17,185 @@ $(document).ready(function () {
 
 let database = firebase.database();
 
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
 //User Authentication Code
 
-function submitCreateAccount() { 
-    var displayName =  document.querySelector("#entry-displayname");
-    var email = document.querySelector("#entry-email");
-    var password = document.querySelector("#entry-password");
-
-    firebase.auth().createUserWithEmailAndPassword(email.value,  password.value)
-        .then(function(user) { 
-            //add the displayName
-            user.updateProfile({displayName: displayName.value});
-        });
-
-}
-
-function signInWithEmailandPassword() { 
-    var email  = document.querySelector("#email");
-    var password = document.querySelector("#password");
-
-    firebase.auth().signInWithEmailandPassword(email.value, password.value);
-}
-
-function googleSignin(googleUser) {
-    var credential = firebase.auth.GoogleAuthProvider.credential({
-        'idToken' : googleUser.getAuthResponse().id_token
-    });
-    firebase.auth().signInWithCredential(credential);
-}
-
-
-firebase.auth().onAuthStateChanged(authStateChangeListener);
-
-function authStateChangeListener(user) {
-    //signin
-    if (user) {
-        //do login operations...
-        Chat.onlogin();
-        Game.onlogin();
-        } else { //signout
-            window.location.reload();    
-        }
-}
-
-
-//Functions for chat messaging
-function sendChatMessage() { 
-    ref = firebase.database().ref("/chat");
-    messageField = document.querySelector("#chat-message");
-
-    ref.push().set({
-        name: firebase.auth().currentUser.displayName,
-        message: messageField.value
-    })
-}
-
-ref = firebase.database().ref("/chat");
-
-ref.on("child_added", function(snapshot) { 
-    var message = snapshot.val();
-    addChatMessage(message.name, message.message);
+$('#logInSubmit').on('click', function(event) {
+    event.preventDefault();
+    submitCreateAccount();
+    $('#popUpWindow').modal('hide');
+    $('#logOutButton').css('display', 'block');
+    $('#logInButton').css('display', 'none');
 });
 
+$('#logOutButton').on('click', function(event) {
+    event.preventDefault();
+    $('#logOutButton').css('display', 'none');
+    $('#logInButton').css('display', 'block');
+});
 
-//Functions for match-making
+ function submitCreateAccount() { 
+     var displayName =  $('#entry-displayname').val().trim();  
+     var email = $('#entry-email').val().trim();
+    var password = $('#entry-password').val().trim();
 
-var STATE = {OPEN: 1, JOINED: 2};
+    console.log(displayName);
+    console.log(email);
+    console.log(password);
 
-// { 
-//     "creator" : {"displayName": "Brennen", "uid": "1234567...1A5DAF6E1"},
-//     "joiner" : {"displayName": "Bob", "uid": "1244567...1A5DAF6E1"},
-//     "state": 2 //JOINED
 
+//     firebase.auth().createUserWithEmailAndPassword(email.value,  password.value)
+//         .then(function(user) { 
+//             //add the displayName
+//             user.updateProfile({displayName: displayName.value});
+//         });
+
+ };
+
+// function signInWithEmailandPassword() { 
+//     var email  = document.querySelector("#email");
+//     var password = document.querySelector("#password");
+
+//     firebase.auth().signInWithEmailandPassword(email.value, password.value);
+// }
+
+// function googleSignin(googleUser) {
+//     var credential = firebase.auth.GoogleAuthProvider.credential({
+//         'idToken' : googleUser.getAuthResponse().id_token
+//     });
+//     firebase.auth().signInWithCredential(credential);
+// }
+
+
+// firebase.auth().onAuthStateChanged(authStateChangeListener);
+
+// function authStateChangeListener(user) {
+//     //signin
+//     if (user) {
+//         //do login operations...
+//         Chat.onlogin();
+//         Game.onlogin();
+//         } else { //signout
+//             window.location.reload();    
+//         }
+// }
+
+
+// //Functions for chat messaging
+// function sendChatMessage() { 
+//     ref = firebase.database().ref("/chat");
+//     messageField = document.querySelector("#chat-message");
+
+//     ref.push().set({
+//         name: firebase.auth().currentUser.displayName,
+//         message: messageField.value
+//     })
+// }
+
+// ref = firebase.database().ref("/chat");
+
+// ref.on("child_added", function(snapshot) { 
+//     var message = snapshot.val();
+//     addChatMessage(message.name, message.message);
+// });
+
+
+// //Functions for match-making
+
+// var STATE = {OPEN: 1, JOINED: 2};
+
+// // { 
+// //     "creator" : {"displayName": "Brennen", "uid": "1234567...1A5DAF6E1"},
+// //     "joiner" : {"displayName": "Bob", "uid": "1244567...1A5DAF6E1"},
+// //     "state": 2 //JOINED
+
+// // };
+
+
+
+
+// ref = firebase.database().ref("/games");
+
+// var openGames = ref.orderByChild("state").equalTo(STATE.OPEN);
+
+// //Function for creating a new game
+
+// function createGame() { 
+//     var user = firebase.auth().currentUser;
+//     var currentGame = {
+//         creator: {uid: user.uid, displayName: user.displayName},
+//         state: STATE.OPEN
+//     };
+
+//     ref.push().set(currentGame);
+// }
+
+
+// //Add join button when someone else adds a new game
+
+// openGames.on("child_added", function(snapshot) {
+//     var data = snapshot.val();
+//     //ignore our own games 
+//     if (data.creator.uid != firebase.auth().currentUser.uid) {
+//         addJoinGameButton(snapshot.key, data);
+//     }
+// });
+
+// //Function for joining a game
+
+// function joinGame(key) { 
+//     var user = firebase.auth().currentUser;
+//     var gameRef = ref.child(key);
+//     gameRef.transaction(function(game) { 
+//         if (!game.joiner) {
+//             game.state = STATE.JOINED;
+//             game.joiner = {uid: user.uid, displayName: user.displayName}
+//         }
+//         return game;
+//     })
+// }
+
+
+// //Removing a game
+
+// openGames.on("child_removed", function(snapshot) {
+//     var item = document.querySelector("#" + snapshot.key);
+//     if (item) {
+//         item.remove();
+//     }
+// });
+
+// function rpsGame(key) {
+//     var gameRef = ref.child(key);
+//     gameRef.on("value", function(snapshot) {
+//         var game = snapshot.val();
+//         switch (game.state) { 
+//             case STATE.JOINED: joinedGame(gameRef, game); break;
+//             // case 
+//             // case
+//             case STATE.COMPLETE: showWinner (game); break; 
+//         }})
 // };
 
 
 
 
-ref = firebase.database().ref("/games");
 
-var openGames = ref.orderByChild("state").equalTo(STATE.OPEN);
-
-//Function for creating a new game
-
-function createGame() { 
-    var user = firebase.auth().currentUser;
-    var currentGame = {
-        creator: {uid: user.uid, displayName: user.displayName},
-        state: STATE.OPEN
-    };
-
-    ref.push().set(currentGame);
-}
-
-
-//Add join button when someone else adds a new game
-
-openGames.on("child_added", function(snapshot) {
-    var data = snapshot.val();
-    //ignore our own games 
-    if (data.creator.uid != firebase.auth().currentUser.uid) {
-        addJoinGameButton(snapshot.key, data);
-    }
-});
-
-//Function for joining a game
-
-function joinGame(key) { 
-    var user = firebase.auth().currentUser;
-    var gameRef = ref.child(key);
-    gameRef.transaction(function(game) { 
-        if (!game.joiner) {
-            game.state = STATE.JOINED;
-            game.joiner = {uid: user.uid, displayName: user.displayName}
-        }
-        return game;
-    })
-}
-
-
-//Removing a game
-
-openGames.on("child_removed", function(snapshot) {
-    var item = document.querySelector("#" + snapshot.key);
-    if (item) {
-        item.remove();
-    }
-});
-
-function rpsGame(key) {
-    var gameRef = ref.child(key);
-    gameRef.on("value", function(snapshot) {
-        var game = snapshot.val();
-        switch (game.state) { 
-            case STATE.JOINED: joinedGame(gameRef, game); break;
-            // case 
-            // case
-            case STATE.COMPLETE: showWinner (game); break; 
-        }})
-};
+ });
 
 
 
+// // Instructions
 
-
-});
-
-
-
-// Instructions
-
-// Create a game that suits this user story:
-// Only two users can play at the same time.
-// Both players pick either rock, paper or scissors. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
-// The game will track each player's wins and losses.
-// Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
-// Styling and theme are completely up to you. Get Creative!
-// Deploy your assignment to Github Pages.
+// // Create a game that suits this user story:
+// // Only two users can play at the same time.
+// // Both players pick either rock, paper or scissors. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
+// // The game will track each player's wins and losses.
+// // Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
+// // Styling and theme are completely up to you. Get Creative!
+// // Deploy your assignment to Github Pages.
 
